84d3380ddb55b84129d8da6ef053f3254bdb5c94
willdrug <whydafuqdoineedthis@gmail.com>
2019-07-29 07:38:37 +0300

    ehhh

--- emojiurl/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class EmojiurlConfig(AppConfig):
-    name = 'emojiurl'
--- main/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render, reverse
-from django.contrib.auth.decorators import login_required
-# Create your views here.
-
-
-def index_view(request):
-    return render(request, 'main/index.html')
