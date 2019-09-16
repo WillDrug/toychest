567044fe0e472d5a6bdba28543044c227e203bb9
WillDrug <whydafuqdoineedthis@gmail.com>
2019-09-16 11:57:30 +0300

    added discovery; broke nginx

--- toychest/src/toychest.py
+++ toychest/src/toychest.py
@@ -2,20 +2,43 @@ import tornado.httpserver
 import tornado.ioloop
 import tornado.options
 import tornado.web
+from time import time
+import requests
 
 from tornado.options import define, options
 
 define("port", default=80, help="run on the given port", type=int)
 
+class ToyDiscoveryHandler:
+    def __init__(self):
+        self.cache_ex = 18700 # todo this and that and those
+        self.cache_time = 0
+        self.cache = {}
+
+    def get_services(self):
+        if (self.cache_time - time()) < (0-self.cache_ex):
+            rs = requests.get('http://toydiscover')
+            self.cache = rs.json()
+        return self.cache
+
+    def report(self):
+        requests.post('http://toydiscover', json={'ver': 1, 'payload': {'name': 'cooldude', 'description': 'WEEE'}})
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
-        self.write(f"Hello, {self.request.uri}!")
+        self.settings['td'].report()  # testing
+
+        srvs = self.settings['td'].get_services()
+        self.write(f"Hello, this is an index!<br>")
+        for srv in srvs:
+            self.write(f"{srv}:<i>{srvs[srv]}</i><br>")
+        self.finish()
+        return
 
 
 def main():
     tornado.options.parse_command_line()
-    application = tornado.web.Application([(r"/?.+", MainHandler)])
+    application = tornado.web.Application([(r"/?.+", MainHandler)], td=ToyDiscoveryHandler())
     http_server = tornado.httpserver.HTTPServer(application)
     http_server.listen(options.port)
     tornado.ioloop.IOLoop.current().start()
---
+++ toydiscover/src/toydiscover.py
@@ -0,0 +1,72 @@
+import tornado.httpserver
+import tornado.ioloop
+import tornado.options
+import tornado.web
+from redis import Redis, RedisError
+import json
+
+from tornado.options import define, options
+
+define("port", default=80, help="run on the given port", type=int)
+
+rd = Redis('redis', 6379, 0, None, decode_responses=True, encoding='utf-8')
+
+class DiscoverHandler(tornado.web.RequestHandler):
+
+    def bad_response(self, message):
+        self.set_status(500)
+        self.write(json.dumps({'success': False, 'message': message}))
+        self.finish()
+        return
+
+    def post(self):
+        try:
+            inb = json.loads(self.request.body)
+        except json.JSONDecodeError:
+            return self.bad_response('Failed to parse inbound message')
+
+        if inb.get('ver') != 1:
+            return self.bad_response('version mismatch')
+
+        inb = inb.get('payload')
+        if inb is None:
+            return self.bad_response('Malformed inbound message')
+
+        name = inb.get('name')
+        desc = inb.get('description')
+
+        st = self.settings['db']
+        try:
+            st.set(name, desc, ex=17800) # todo expiry, dynamic names
+        except RedisError:
+            return self.bad_response('Database down')
+
+        self.write(json.dumps({'success': True, 'message': 'registered'}))
+        self.set_status(200)
+        self.finish()
+        return
+
+    def get(self):
+        st = self.settings['db']
+        try:
+            ks = st.keys('*')
+            ret = {k:rd.get(k) for k in ks}
+        except RedisError:
+            return self.bad_response('Database down')
+
+        self.set_status(200)
+        self.write(json.dumps(ret))
+        self.finish()
+        return
+
+
+def main():
+    tornado.options.parse_command_line()
+    application = tornado.web.Application([(r"/?.+", DiscoverHandler)], db=rd)
+    http_server = tornado.httpserver.HTTPServer(application)
+    http_server.listen(options.port)
+    tornado.ioloop.IOLoop.current().start()
+
+
+if __name__ == "__main__":
+    main()
