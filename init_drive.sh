621fd1ba943be505a84f0f4cc0fd881551bfee9e
willdrug <whydafuqdoineedthis@gmail.com>
2023-01-10 12:22:42 +0100

    repeated drive config update

--- init_drive.sh
+++ init_drive.sh
@@ -1,4 +1,6 @@
 # run with user, password, secret for an auth flow.
 # expecting hostname to be valid for a callback url, though.
 # can use -e drive_host=127.0.0.1
-docker run --rm -ti --network toychest_toysupport -e username=$1 -e password=$2 -e init_drive=true -p 8080:8080 --entrypoint python -e drive_secret=$3 toydiscover:1.0.0 modify_config.py
+# $4 -> {'config_file_id': '....'} folder id
+docker run --rm -ti --network toychest_toysupport -e username=$1 -e password=$2 -e init_drive=true -p 8080:8080 --entrypoint python -e drive_secret=$3 toydiscover:1.0.0 modify_config.py --config_json $4
+docker restart toydiscover
--- toydiscover/src/discoverapi.py
+++ toydiscover/src/discoverapi.py
@@ -3,16 +3,21 @@ from toycommons import ToyInfra
 from toycommons.model.service import Service
 from time import time
 from os import getenv
+from fastapi_utils.tasks import repeat_every
 import json
+import threading
+
+
 
 def prepare_app(tc):
     app = FastAPI()
 
     service_data = {}  # non-db held service registry. If scaled, update to use db
+
     def clean():
         nonlocal service_data
         service_data = {k: service_data[k] for k in service_data
-                        if time()-service_data[k]['reported'] < tc.config.discovery_ttl + 5}  # todo: config up grace
+                        if time() - service_data[k]['reported'] < tc.config.discovery_ttl + 5}  # todo: config up grace
 
     @app.post("/services")
     async def update_service(service: Service):
@@ -25,7 +30,16 @@ def prepare_app(tc):
         clean()
         return [q['service'] for q in service_data.values()]
 
+    @app.on_event("startup")
+    @repeat_every(seconds=tc.config.drive_config_sync_ttl)
+    async def update_config():
+        config = tc.drive.file_by_id(tc.config.config_file_id)
+        config = json.loads(config.decode())  # todo error dodge?
+        for k in config:
+            tc.config.set_any(k, config[k])
+
     return app
+
 
 tc = ToyInfra(user='root', passwd='qwerty')  # todo env
 app = prepare_app(tc)
