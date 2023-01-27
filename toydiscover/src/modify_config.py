e3e9f271cd4d1de6035876b02a590440ecfcd785
willdrug <whydafuqdoienedthis@gmail.com>
2023-01-27 18:36:25 +0100

    small fix

--- toydiscover/src/modify_config.py
+++ toydiscover/src/modify_config.py
@@ -1,11 +1,10 @@
-from toycommons.config import Config
+from toycommons.storage.config import Config
 from toycommons.drive import DriveConnect
 from pymongo import MongoClient
 from os import getenv
 import json
 from google_auth_oauthlib.flow import InstalledAppFlow
-# fixme this is bullshit :\
-
+from toycommons import ToyInfra
 
 if __name__ == '__main__':
     host = getenv('host', 'mongodb://toydb')
@@ -17,11 +16,9 @@ if __name__ == '__main__':
     drive_secret = getenv('drive_secret')
     config_json = getenv('config_json')
 
+    infra = ToyInfra('admin', host=host, port=port, user=username, passwd=password)
 
-    client = MongoClient(host=host, port=port, username=username, password=password)
-    config = Config(client.toyinfra.mainline)
-
-    host_callback = getenv('drive_host', config.base_url)
+    host_callback = getenv('drive_host', infra.config.base_url).replace('https://', '').replace('http://', '')
 
     if init_drive:
         if drive_secret is None:
@@ -29,8 +26,9 @@ if __name__ == '__main__':
         secret = json.loads(drive_secret)
         flow = InstalledAppFlow.from_client_config(secret, DriveConnect.SCOPES)
         creds = flow.run_local_server(port=drive_port, host=host_callback, bind_addr='0.0.0.0')
-        config.drive_token = json.loads(creds.to_json())
+        print(creds.to_json())
+        infra.config.drive_token = json.loads(creds.to_json())
     if config_json:
         mod = json.loads(config_json)
         for k in mod:
-            config[k] = mod[k]
+            infra.config[k] = mod[k]
