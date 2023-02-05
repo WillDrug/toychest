6e6a1530a5297041f1ea7b61960c1c9686b3ecdd
willdrug <whydafuqdoienedthis@gmail.com>
2023-02-05 22:49:08 +0100

    added no-cache storage get method

--- init_drive.sh
+++ init_drive.sh
@@ -2,5 +2,9 @@
 # expecting hostname to be valid for a callback url, though.
 # can use -e drive_host=127.0.0.1
 # $4 -> {'config_file_id': '....'} folder id
-docker run --rm -ti --network toychest_toysupport -e username=$1 -e password=$2 -e init_drive=true -p 8080:8080 --entrypoint python -e drive_secret=$3 toydiscover:1.0.0 modify_config.py --config_json $4
+
+docker create --name modify --network toychest_toysupport --hostname modify -e username=$1 -e password=$2 -e init_drive=true -p 8080:8080 --entrypoint python -e drive_secret=$3 toydiscover:1.0.0 modify_config.py --config_json $4
+docker network connect toynet modify
+docker start -i modify
+docker rm modify
 docker restart toydiscover
--- toydiscover/src/modify_config.py
+++ toydiscover/src/modify_config.py
@@ -1,10 +1,46 @@
 from toycommons.storage.config import Config
 from toycommons.drive import DriveConnect
 from pymongo import MongoClient
+from googleapiclient.discovery import build
 from os import getenv
 import json
 from google_auth_oauthlib.flow import InstalledAppFlow
 from toycommons import ToyInfra
+import google_auth_oauthlib
+import flask
+from toycommons.drive import DriveConnect
+
+
+app = flask.Flask('authenticator')
+
+
+
+def credentials_to_dict(credentials):
+    return {'token': credentials.token,
+            'refresh_token': credentials.refresh_token,
+            'token_uri': credentials.token_uri,
+            'client_id': credentials.client_id,
+            'client_secret': credentials.client_secret,
+            'scopes': credentials.scopes,
+            'id_token': credentials.id_token}
+
+@app.route("/")
+def oauth2callback():
+    # Receive an authorisation code from google
+    flow = google_auth_oauthlib.flow.Flow.from_client_config(json.loads(drive_secret), scopes=DriveConnect.SCOPES)
+    flow.redirect_uri = host_callback
+    authorization_response = flask.request.url.replace('http://', 'https://')
+    # Use authorisation code to request credentials from Google
+    flow.fetch_token(authorization_response=authorization_response)
+    credentials = flow.credentials
+    credentials_to_dict(credentials)
+    # Use the credentials to obtain user information and save it to the session
+    # oauth2_client = build('oauth2','v2',credentials=credentials)
+    # user_info = oauth2_client.userinfo().get().execute()
+    infra.config.drive_token = credentials_to_dict(credentials)
+    print('done, ctrl-c out of this.')
+    return None
+
 
 if __name__ == '__main__':
     host = getenv('host', 'mongodb://toydb')
@@ -16,18 +52,23 @@ if __name__ == '__main__':
     drive_secret = getenv('drive_secret')
     config_json = getenv('config_json')
 
-    infra = ToyInfra('admin', host=host, port=port, user=username, passwd=password)
-
-    host_callback = getenv('drive_host', infra.config.base_url).replace('https://', '').replace('http://', '')
-
+    infra = ToyInfra('admin', host=host, port=port, user=username, passwd=password, drive=False)
     if init_drive:
         if drive_secret is None:
             raise ValueError(f'Provide drive_secret')
+        host_callback = getenv('drive_host', infra.config.base_url) + '/.auth/'
         secret = json.loads(drive_secret)
-        flow = InstalledAppFlow.from_client_config(secret, DriveConnect.SCOPES)
-        creds = flow.run_local_server(port=drive_port, host=host_callback, bind_addr='0.0.0.0')
-        print(creds.to_json())
-        infra.config.drive_token = json.loads(creds.to_json())
+        flow = google_auth_oauthlib.flow.Flow.from_client_config(json.loads(drive_secret), scopes=DriveConnect.SCOPES)
+        flow.redirect_uri = host_callback
+        authorization_url, state = flow.authorization_url(access_type='offline', include_granted_scopes='true')
+        print(f'Please visit {authorization_url}')
+        try:
+            app.run('0.0.0.0', drive_port)
+        except KeyboardInterrupt:
+            print('turning server off')
+        # flow = InstalledAppFlow.from_client_config(secret, DriveConnect.SCOPES)
+        # creds = flow.run_local_server(port=drive_port, host=host_callback, bind_addr='0.0.0.0')
+        # infra.config.drive_token = json.loads(creds.to_json())
     if config_json:
         mod = json.loads(config_json)
         for k in mod:
