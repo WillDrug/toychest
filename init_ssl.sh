745605e75268602bb400e9bbc0ca1f0317d35aa7
willdrug <whydafuqdoienedthis@gmail.com>
2023-02-06 10:40:48 +0100

    oof

--- init_ssl.sh
+++ init_ssl.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(willdrug.me www.willdrug.me)
+domains=(willdrug.me *.willdrug.me)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="$1"
