4b929b24d1fa3b84ab283f1c64f03d892fbed811
Sergei Bobkov <whydafuqdoineedthis@gmail.com>
2023-02-06 15:29:54 +0000

    ..

---
+++ data/certbot/conf/acme-dns-auth.sh
@@ -0,0 +1,1 @@
+python /etc/letsencrypt/acme-dns-auth.py
---
+++ data/certbot/conf/first_run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+certbot certonly -w /var/www/certbot \
+    --manual --manual-auth-hook /etc/letsencrypt/acme-dns-auth.sh --preferred-challenges dns \
+    --debug-challenges --test-cert \
+    --email $email \
+    -d $domain_main \
+    -d $domain_wild \
+    --agree-tos
+
