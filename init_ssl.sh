61cc47d2ddd9a5ef0e7abe7bd1e522cd18502be9
Sergei Bobkov <whydafuqdoineedthis@gmail.com>
2023-02-06 15:44:48 +0000

    ...

--- init_ssl.sh
+++ init_ssl.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 source .env
+rm -rf data/certbot/conf/live/willdrug.me
 docker-compose run --rm --env email --env domain --entrypoint "sh" certbot /etc/letsencrypt/first_run.sh
 
 echo "### Reloading nginx ..."
