e08f50d939ff7473413d687903e1477c8b767ad8
Sergei Bobkov <whydafuqdoineedthis@gmail.com>
2023-02-06 12:25:37 +0000

    fix ssl init

--- init_ssl.sh
+++ init_ssl.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-docker-compose run --rm --entrypoint "\
-  certbot certonly --webroot -w /var/www/certbot \
-    --manual --manual-auth-hook /etc/letsencrypt/acme-dns-auth.py --preferred-challenges dns
-    --email $email \
-    -d $domain_main \
-    -d $domain_wild \
-    --agree-tos \
-    --force-renewal" certbot
+docker-compose run --rm -e email="$email" -e domain_main="$domain_main" -e domain_wild="$domain_wild" --entrypoint "\
+  certbot certonly -w /var/www/certbot \
+    --manual --manual-auth-hook /etc/letsencrypt/acme-dns-auth.py --preferred-challenges dns \
+    --email \$email \
+    -d \$domain_main \
+    -d \$domain_wild \
+    --agree-tos" certbot
 echo
 
 echo "### Reloading nginx ..."
