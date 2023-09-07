42a7894b87909999240c8578c63a3e8b1e9745c7
willdrug <whydafuqdoienedthis@gmail.com>
2023-09-07 21:10:03 +0200

    init_ssl updated for DO token usage

--- init_ssl.sh
+++ init_ssl.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 source .env
-rm -rf data/certbot/conf/live/willdrug.me
-docker-compose run --rm --env email --env domain --entrypoint "sh" certbot /etc/letsencrypt/first_run.sh
+#rm -rf data/certbot/conf/live/willdrug.me
+docker-compose run --rm --env email --env domain --env main_domain --env DIGITALOCEAN_TOKEN --entrypoint "sh" certbot /etc/letsencrypt/first_run.sh
 
 echo "### Reloading nginx ..."
 docker-compose exec toynginx nginx -s reload
