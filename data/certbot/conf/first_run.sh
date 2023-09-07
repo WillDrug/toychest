a1ed83d529a85c029b94e033f5c279bf42ac682a
willdrug <whydafuqdoienedthis@gmail.com>
2023-09-07 20:39:17 +0200

    testing digitalocean-dns certbot

--- data/certbot/conf/first_run.sh
+++ data/certbot/conf/first_run.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
-certbot certonly -w /var/www/certbot \
-    --manual --manual-auth-hook /etc/letsencrypt/acme-dns-auth.sh --preferred-challenges dns \
-    --debug-challenges \
-    --email $email \
-    -d $domain \
-    --agree-tos
+# no digitalocean acme auth hook
+#certbot certonly -w /var/www/certbot \
+#    --manual --manual-auth-hook /etc/letsencrypt/acme-dns-auth.sh --preferred-challenges dns \
+#    --debug-challenges \
+#    --email $email \
+#    -d $domain \
+#    --agree-tos
 
-certbot certonly -w /var/www/certbot --domain $main_domain --agree-tos --preferred-challenges http
+#certbot certonly -w /var/www/certbot --domain $main_domain --agree-tos --preferred-challenges http
+
+
+# digital ocean token usage
+echo "dns_digitalocean_token = $DIGITALOCEAN_TOKEN" > creds.ini
+chmod go-rwx creds.ini
+sudo certbot certonly --dns-digitalocean --dns-digitalocean-credentials creds.ini -d $main_domain -d $domain
