0099275eb5825249a15345d354f063fdf0700723
willdrug <whydafuqdoienedthis@gmail.com>
2024-03-07 17:23:32 +0100

    discover switch

--- toydiscover/src/discoverapi.py
+++ toydiscover/src/discoverapi.py
@@ -31,11 +31,11 @@ def prepare_app(tc):
         return [q['service'] for q in service_data.values()]
 
     @app.on_event("startup")
-    @repeat_every(seconds=tc.config.drive_config_sync_ttl)  # todo: change to discovery_sync_timeout
+    @repeat_every(seconds=tc.config.drive_config_sync_ttl)
     async def update_config():
         # tc.get_own_config(ignore_cache=True) -> into tc.config
         config = tc.drive.file_by_id(tc.config.config_file_id)
-        config = json.loads(config.decode())  # todo error dodge?
+        config = json.loads(config.decode())
         for k in config:
             tc.config[k] = config[k]
 
